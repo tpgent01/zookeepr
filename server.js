2e55e7ae1b87758ced42c76a98df5c61384d5103
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-02-25 16:15:48 -0600

    added middleware so app can accept POST data

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
   let filteredResults = animalsArray;
@@ -51,6 +56,12 @@ app.get('/api/animals/:id', (req, res) => {
   } else {
     res.send(404);
   }
+});
+
+app.post('/api/animals', (req, res) => {
+  // req.body is where our incoming content will be
+  console.log(req.body);
+  res.json(req.body);
 });
 
 app.listen(PORT, () => {
