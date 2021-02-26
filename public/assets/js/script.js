ad1bcd6949279067f8987d2b7dee12763b49dc22
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-02-26 16:20:52 -0600

    finished GET requests to serve page data

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,5 @@
 const $animalForm = document.querySelector('#animal-form');
+const $zookeeperForm = document.querySelector('#zookeeper-form');
 
 const handleAnimalFormSubmit = event => {
   event.preventDefault();
@@ -38,13 +39,12 @@ const handleAnimalFormSubmit = event => {
       if (response.ok) {
         return response.json();
       }
-      alert('Error: ' + response.statusText);
+      alert(`Error: ${response.statusText}`);
     })
     .then(postResponse => {
       console.log(postResponse);
       alert('Thank you for adding an animal!');
     });
-
 };
 
 $animalForm.addEventListener('submit', handleAnimalFormSubmit);
