27d330d5dbb54926902a2ab7ea0a5361a531ac54
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-02-26 16:12:24 -0600

    created routes to serve other pages

--- server.js
+++ server.js
@@ -106,6 +106,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
